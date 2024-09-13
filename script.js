1cbef2baa5330f84317bd23a8e4567b202b43edb
Nathan-network <vinoynathan@outlook.fr>
2024-09-13 17:17:02 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const btn = document.querySelector("button")
 
-btn.addEventListener("click",(e)=>{
-    console.log(e.target)
-    e.target.innerText = "salut clique"
+btn.addEventListener("click",()=>{
+document.querySelector('h2').style.color = "orange"
+document.querySelector('h1').innerHTML = "h2"
+document.querySelector('h2').innerText = "bouton clicker"
 })
 
 console.log(btn)
